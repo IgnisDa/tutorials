f06a26e75bef25c3ed387eab45b82502b99d12bb
IgnisDa <ignisda2002@gmail.com>
2020-10-15 15:55:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tutorials
+
+This repository contains all the tutorials I have written. They will made available soon on http://IgnisDa.me in the near future.
---
+++ how-to-get-tailwind-working-with-nuxt/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    <div class="title">This is awesome!</div>
+    <div class="text-red-700">Yeah it all works!</div>
+    <p class="hover:bg-green-600">Testing hover text</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.title {
+  font-size: 50px;
+}
+</style>
