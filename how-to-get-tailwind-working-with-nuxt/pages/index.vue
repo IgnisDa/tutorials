be4a7b42c0fdc0424879271d3cd3b98fe705b542
IgnisDa <ignisda2002@gmail.com>
2020-10-15 16:15:29 +0530

    add styles to main.css and modify pages/index.vue accordingly

--- how-to-get-tailwind-working-with-nuxt/pages/index.vue
+++ how-to-get-tailwind-working-with-nuxt/pages/index.vue
@@ -3,6 +3,7 @@
     <div class="title">This is awesome!</div>
     <div class="text-red-700">Yeah it all works!</div>
     <p class="hover:bg-green-600">Testing hover text</p>
+    <a href="http://github.com/IgnisDa">Profile</a>
   </div>
 </template>
 
