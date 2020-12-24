19658e0de2883e944eaf028ad01e1ec3f34cfd86
IgnisDa <ignisda2002@gmail.com>
2020-12-24 22:49:56 +0530

    feat(django_pytest_tutorial/settings.py): add notes app

--- django_pytest_tutorial/django_pytest_tutorial/settings.py
+++ django_pytest_tutorial/django_pytest_tutorial/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User installed apps
+    'notes.apps.NotesConfig',
+
+    # Django addons
 ]
 
 MIDDLEWARE = [
@@ -86,7 +91,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
