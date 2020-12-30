a1b469b02673d42461c4533226bcfd707ab7f3d2
IgnisDa <ignisda2002@gmail.com>
2020-12-30 20:02:34 +0530

    test(tests/accounts/test_models.py): invalid_email

--- django_pytest_tutorial/tests/accounts/test_models.py
+++ django_pytest_tutorial/tests/accounts/test_models.py
@@ -1,4 +1,25 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+
 class TestCustomUser:
 
-    def test_created(self):
-        assert 0 == 1
+    @pytest.mark.django_db
+    def test_created_version_one(self):
+        """ This is just an example of a very basic test. You would probably want to
+        write a pytest fixture for creating a user instance. """
+        CustomUser.objects.create_user(email='test@email.com', password='test-password')
+        assert CustomUser.objects.count() == 1
+
+    def test_created_version_two(self, create_custom_user):
+        """ The above test but using a pytest fixture. """
+        create_custom_user(email='test@email.com', password='test-password')
+        assert CustomUser.objects.count() == 1
+
+    def test_value_error_on_invalid_email(self, create_custom_user):
+        """ Test that a ValueError is raised if we supply an invalid email ID. """
+        with pytest.raises(ValueError):
+            create_custom_user(email='incorrect-email', password='test-password')
+        assert CustomUser.objects.count() == 0
