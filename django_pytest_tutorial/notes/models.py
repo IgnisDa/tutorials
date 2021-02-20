bb5657ca96c0ca929c7f4cb9db2897cfe19cf9bd
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-20 15:05:43 +0550

    refactor(models.py): lint file

--- django_pytest_tutorial/notes/models.py
+++ django_pytest_tutorial/notes/models.py
@@ -3,21 +3,16 @@ from django.utils.translation import gettext as _
 
 
 class Note(models.Model):
-    title = models.CharField(
-        max_length=200, help_text=_('The title for the note.')
-    )
+    title = models.CharField(max_length=200, help_text=_("The title for the note."))
     summary = models.TextField(
-        blank=True, null=True,
-        help_text=_('Briefly describe your note.')
+        blank=True, null=True, help_text=_("Briefly describe your note.")
     )
-    text = models.TextField(
-        help_text=_('The actual note you want to write.')
-    )
+    text = models.TextField(help_text=_("The actual note you want to write."))
     owner = models.ForeignKey(
-        'accounts.CustomUser', related_name='note', on_delete=models.CASCADE
+        "accounts.CustomUser", related_name="note", on_delete=models.CASCADE
     )
     created_on = models.DateTimeField(
-        auto_now_add=True, help_text=_('The time when this note was created.')
+        auto_now_add=True, help_text=_("The time when this note was created.")
     )
 
     def __str__(self):
